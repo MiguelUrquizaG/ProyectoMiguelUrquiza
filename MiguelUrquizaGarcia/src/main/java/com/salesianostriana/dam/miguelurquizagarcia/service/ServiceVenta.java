810d9eefe64e9acb7f1793dd2852307673204cb8
Miguel Urquiza García <miguelurquiza.contacto@gmail.com>
2025-05-14 13:32:37 +0200

    No he tocado mucho

--- MiguelUrquizaGarcia/src/main/java/com/salesianostriana/dam/miguelurquizagarcia/service/ServiceVenta.java
+++ MiguelUrquizaGarcia/src/main/java/com/salesianostriana/dam/miguelurquizagarcia/service/ServiceVenta.java
@@ -1,5 +1,7 @@
 package com.salesianostriana.dam.miguelurquizagarcia.service;
 
+import java.util.OptionalDouble;
+
 import org.springframework.stereotype.Service;
 
 import com.salesianostriana.dam.miguelurquizagarcia.model.LineaVenta;
@@ -12,8 +14,26 @@ public class ServiceVenta extends BaseService<Venta, Long, VentaRepository>{
 	public void saveVenta(Venta nuevaVenta) {
 		for(LineaVenta linea: nuevaVenta.getLineasVenta()) {
 			linea.setVenta(nuevaVenta);
+			System.out.println(linea.getSubTotal());
 		}
+		nuevaVenta.setPrecioTotal(calcularPrecioTotalVenta(nuevaVenta));
 		save(nuevaVenta);
+	}
+	
+	public double calcularPrecioTotalVenta(Venta v) {
+//		OptionalDouble total = v.getLineasVenta().stream()
+//		.mapToDouble(l -> l.getSubTotal())
+//		.average();
+//		System.out.println(total);
+//		return total.getAsDouble();
+		
+		double total=0;
+		for(LineaVenta l: v.getLineasVenta()) {
+			
+			total+=l.getSubTotal();
+		}
+		
+		return total;
 	}
 	
 }
