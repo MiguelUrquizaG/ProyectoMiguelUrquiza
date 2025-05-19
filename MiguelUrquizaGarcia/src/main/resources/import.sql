0146670ca4f3666d3f48ea2a07e3ac586282d003
MiguelUrquiza <miguelurquiza.contacto@gmail.com>
2025-05-19 20:54:52 +0200

    Retoques Visuales

--- MiguelUrquizaGarcia/src/main/resources/import.sql
+++ MiguelUrquizaGarcia/src/main/resources/import.sql
@@ -8,14 +8,14 @@ ALTER TABLE categoria ALTER COLUMN id RESTART WITH 6;
 
 
 -- Insertar prendas
-INSERT INTO prenda (id, nombre, descripcion, url_imagen, top_venta, categoria_id) VALUES (1, 'Traje formal', 'Traje de dos piezas para ocasiones especiales', 'img/traje-formal.jpg', true, 1);
-INSERT INTO prenda (id, nombre, descripcion, url_imagen, top_venta, categoria_id) VALUES (2, 'Vestido de seda', 'Vestido elegante de seda natural', 'img/vestido-seda.jpg', false, 1);
-INSERT INTO prenda (id, nombre, descripcion, url_imagen, top_venta, categoria_id) VALUES (3, 'Camisa de algodón', 'Camisa para uso diario', 'img/camisa-algodon.jpg', true, 2);
-INSERT INTO prenda (id, nombre, descripcion, url_imagen, top_venta, categoria_id) VALUES (4, 'Pantalón de lino', 'Pantalón ligero para verano', 'img/pantalon-lino.jpg', false, 2);
-INSERT INTO prenda (id, nombre, descripcion, url_imagen, top_venta, categoria_id) VALUES (5, 'Abrigo de lana', 'Abrigo grueso para invierno con mancha de café', 'img/abrigo-lana.jpg', true, 3);
-INSERT INTO prenda (id, nombre, descripcion, url_imagen, top_venta, categoria_id) VALUES (6, 'Chaqueta de ante', 'Chaqueta con manchas de grasa', 'img/chaqueta-ante.jpg', false, 3);
-INSERT INTO prenda (id, nombre, descripcion, url_imagen, top_venta, categoria_id) VALUES (7, 'Vaqueros desgastados', 'Vaqueros para renovar con tinte', 'img/vaqueros-desgastados.jpg', true, 4);
-INSERT INTO prenda (id, nombre, descripcion, url_imagen, top_venta, categoria_id) VALUES (8, 'Blusa descolorida', 'Blusa para recuperar color original', 'img/blusa-descolorida.jpg', false, 4);
-INSERT INTO prenda (id, nombre, descripcion, url_imagen, top_venta, categoria_id) VALUES (9, 'Camiseta deportiva', 'Camiseta para ejercicio', 'img/camiseta-deportiva.jpg', true, 5);
-INSERT INTO prenda (id, nombre, descripcion, url_imagen, top_venta, categoria_id) VALUES (10, 'Falda plisada', 'Falda con pliegues elegantes', 'img/falda-plisada.jpg', false, 5);
+INSERT INTO prenda (id, nombre, descripcion, url_imagen, top_venta, categoria_id) VALUES (1, 'Traje formal', 'Traje de dos piezas para ocasiones especiales', 'https://img.joomcdn.net/edd88594fc8b91ee8f308f6ada845f403db4bba7_original.jpeg', true, 1);
+INSERT INTO prenda (id, nombre, descripcion, url_imagen, top_venta, categoria_id) VALUES (2, 'Vestido de seda', 'Vestido elegante de seda natural', 'https://lagatavoladora.com/wp-content/uploads/2024/07/vestido-de-seda-celeste-Oshun-1.jpg', false, 1);
+INSERT INTO prenda (id, nombre, descripcion, url_imagen, top_venta, categoria_id) VALUES (3, 'Camisa de algodón', 'Camisa para uso diario', 'https://www.vestuariolaboral.com/82032-large_default/camisa-algodon-manga-larga-velilla-533.jpg', true, 2);
+INSERT INTO prenda (id, nombre, descripcion, url_imagen, top_venta, categoria_id) VALUES (4, 'Pantalón de lino', 'Pantalón ligero para verano', 'https://www.lidl.es/media/product/0/0/5/6/6/5/9/pantalon-de-lino-para-mujer-zoom--5.jpg', false, 2);
+INSERT INTO prenda (id, nombre, descripcion, url_imagen, top_venta, categoria_id) VALUES (5, 'Abrigo de lana', 'Abrigo grueso para invierno con mancha de café', 'https://static.zara.net/assets/public/6fe9/4859/29994a44ace6/a7a8d61ffa9d/01008020442-e1/01008020442-e1.jpg?ts=1733328153443&w=1024', true, 3);
+INSERT INTO prenda (id, nombre, descripcion, url_imagen, top_venta, categoria_id) VALUES (6, 'Chaqueta de ante', 'Chaqueta con manchas de grasa', 'https://e00-telva.uecdn.es/assets/multimedia/imagenes/2024/10/07/17283010323767.jpg', false, 3);
+INSERT INTO prenda (id, nombre, descripcion, url_imagen, top_venta, categoria_id) VALUES (7, 'Vaqueros desgastados', 'Vaqueros para renovar con tinte', 'https://www.dhresource.com/webp/m/0x0/f2/albu/g10/M00/EC/A3/rBVaWV6YGBCAFBjhAAIKRf8URv4764.jpg', true, 4);
+INSERT INTO prenda (id, nombre, descripcion, url_imagen, top_venta, categoria_id) VALUES (8, 'Camiseta descolorida', 'Blusa para recuperar color original', 'https://image.made-in-china.com/155f0j00ZbkqHWSnMEcs/Custom-Oversize-T-Shirt-Manufacturer-Casual-Vintage-Heavyweight-Cotton-Acid-Wash-Sun-Faded-T-Shirts.webp', false, 4);
+INSERT INTO prenda (id, nombre, descripcion, url_imagen, top_venta, categoria_id) VALUES (9, 'Camiseta deportiva', 'Camiseta para ejercicio', 'https://printalba.com/wp-content/uploads/2021/06/Delantero-Milan.jpg', true, 5);
+INSERT INTO prenda (id, nombre, descripcion, url_imagen, top_venta, categoria_id) VALUES (10, 'Falda plisada', 'Falda con pliegues elegantes', 'https://img.ltwebstatic.com/images3_spmp/2025/02/13/37/1739431020c1c604e9d2d6d6a47d339554fe198826_thumbnail_405x.webp', false, 5);
 ALTER TABLE prenda ALTER COLUMN id RESTART WITH 11;
